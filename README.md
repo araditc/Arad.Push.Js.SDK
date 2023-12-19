920f9f7ab4b6dfed5ef9948015fe43f26893cff1
metadolism <metadolism@gmail.com>
2023-12-19 03:38:39 +0330

    update reame - set public and private

--- README.md
+++ README.md
@@ -1,29 +1,67 @@
-# Arad Push Notification system (APN)
+![](https://i.postimg.cc/t4PCNhgB/apn.png)
 
-**APN** documentation for node.js applications. It can be use in **ReactJS**, **VueJS** or **AngularJS**.
+# Arad Push Notification system (APN)
+You can use this sdk in node.js applications such as **ReactJS**, **VueJS** or **AngularJS**.
+
+> ## Before start (IMPORTANT)
+> You need The **firebase configuration object** and **vapidKey** before initialize APN sdk.
+> ### — Firebase Configuration
+> 1. **Create a Firebase project:** Open the [Firebase console](https://console.firebase.google.com) and click on **“Add project”**. Follow the instructions to create your project.
+> 2. **Get your Firebase configuration:** After your project is created, click on the gear icon next to **“Project Overview”** and select **“Project settings”**. Here, you’ll find your Firebase SDK snippet under the **“General”** tab. It will look something like this:
+>```js
+>const firebaseConfig = {
+>  apiKey: "AIzaSy...",
+>  authDomain: "your-project-id.firebaseapp.com",
+>  databaseURL: "https://your-project-id.firebaseio.com",
+>  projectId: "your-project-id",
+>  storageBucket: "your-project-id.appspot.com",
+>  messagingSenderId: "sender-id",
+>  appId: "app-id",
+>  measurementId: "G-measurement-id",
+>};
+>```
+> ### — VAPID Key
+> 1. **Enable Cloud Messaging in your Firebase project:** In the Firebase console, navigate to “Cloud Messaging” under the “Grow” section. Here, you can enable Firebase Cloud Messaging for your project.
+> 2. **Get your VAPID key:** After enabling Cloud Messaging, you’ll find your **VAPID key** under the **“Cloud Messaging”** settings. It will be labeled as **“Web Push certificates”**. It will look something like this:
+> ```js
+> const vapidkey = 'BB0rUaCvQVl1NA9sENxz9y...'
+> ```
+
 
 ## Installation
+Always get the latest version with the following command:
+```js
+npm install met-generate-int
+```
 
-Always get the latest version with the following code
-`npm install met-generate-int`
+## Import
+```js
+import APN from ‘met-generate-int’
+```
 
 ## How to use
-
-### Import
-
-`import APN from ‘met-generate-int’`
-
-### usage
-
-Create an object of APN In your `App.js` (in react) or `App.vue` (in vuejs) and start coding
+After getting the `firebaseConfig` and `vapidKey` try to create an object of APN in your `App.js` (react) or `App.vue` (vuejs) and start coding:
 ```js
+
+  const firebaseConfig = {
+    apiKey: "AIzaSy...",
+    authDomain: "your-project-id.firebaseapp.com",
+    databaseURL: "https://your-project-id.firebaseio.com",
+    projectId: "your-project-id",
+    storageBucket: "your-project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appId: "app-id",
+    measurementId: "G-measurement-id",
+  };
+  const vapidkey = 'BB0rUaCvQVl1NA9sENxz9y...';
+
+  // create apn object from APN
   const apn = new APN();
 
   //  initialize apn ↓
   apn.init(firebaseConfig, vapidKey).then(() => {
     console.log('Initialize successful');
     apn.setKey('arad-secret-key');
-
     if (!apn.checkConfig()) {
       apn.setConfig('clientUserName', 'clientPassword', 'connectionURL');
     }
@@ -37,20 +75,51 @@ Create an object of APN In your `App.js` (in react) or `App.vue` (in vuejs) and 
     console.log('notification: ', message);
     // do something with notification data
   });
-
 ```
+
+> ## Background Notification
+> The `firebase-messaging-sw.js` file is a **service worker** file used by Firebase Cloud Messaging (FCM) to handle the receiving and displaying of push notifications in the **background**, **even when your web app is not currently open in the browser**.
+> Here’s how you can use it in your web projects:
+> 1. **Create the Service Worker File:** Create a new file named `firebase-messaging-sw.js` in the `public` folder of your project. The public directory is the root of your server, and the service worker file must be located at the root level to have control over all the pages of your site.
+> 2. **Initialize Firebase in the Service Worker:** In the `firebase-messaging-sw.js` file, you’ll need to initialize Firebase and the messaging service. Here’s a basic example:
+>```js
+>// Import and configure Firebase
+>importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+>importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+>
+>const firebaseConfig = {
+>  apiKey: "AIzaSy...",
+>  authDomain: "your-project-id.firebaseapp.com",
+>  databaseURL: "https://your-project-id.firebaseio.com",
+>  projectId: "your-project-id",
+>  storageBucket: "your-project-id.appspot.com",
+>  messagingSenderId: "sender-id",
+>  appId: "app-id",
+>  measurementId: "G-measurement-id",
+>};
+>
+>// Retrieve an instance of Firebase Messaging
+>// so that it can handle background messages.
+>const messaging = firebase.messaging();
+>
+>messaging.onBackgroundMessage((payload) => {
+>  self.registration.showNotification(notificationTitle,
+>    notificationOptions);
+>});
+>
+>```
 
 ## Methods 
 
 | Method | Info |
 | ------------- | ------------- |
-| `init(firebaseConfig, vapidKey)`  | create a firebase account and get your `firebaseConfig` (object) and `vapidKey` (string) (promise) |
+| `init(firebaseConfig, vapidKey)`  | initialize sdk width `firebaseConfig` (object) and `vapidKey` (string) `promise` |
 | `setKey(key)` | set a **secretKey** to ecnrypt configs `void` |
 | `checkConfig()` | check if configs **defined** and **valid** `boolean` |
 | `setConfig()` | **set** connection data `void` |
 | `getToken()` | return firebase token `string` (return `null` before initilize complete) |
-| `getOs()` | return OS name `string` |
-| `getBrowser()` | return browser name `string` |
+| `getOs()` | returns OS name `string` |
+| `getBrowser()` | returns browser name `string` |
 
 
 ## Events
--- index.js
+++ index.js
@@ -10,197 +10,199 @@ const CryptoJS = require("crypto-js");
 class Arad extends EventEmitter {
   constructor() {
     super();
-    this.fcm = null;
-    this.initing = false;
-    this.config = null;
-    this.broker = '';
+    // private data and functions
+    let fcm = null;
+    let initing = false;
+    let broker = '';
     // this.broker = 'wss://o77efe7a.ala.us-east-1.emqxsl.com:8084/mqtt';
-    this.mqttTopic = 'omid';
-    this.client = null;
-    this.secretKey = null;
-  }
-  // 1. init fb
-  init(firebaseConfig, vapidkey) {
-    return new Promise((resolve, reject) => {
-      if (!this.initing) {
-        this.initing = true;
-        const app = initializeApp(firebaseConfig);
-        Notification.requestPermission().then((permission) => {
-          if (permission === 'granted') {
-            const messaging = getMessaging();
-            getToken(messaging, { vapidKey: vapidkey }).then((currentToken) => {
-              if (currentToken) {
-                this.fcm = currentToken;
-                resolve(this.fcm);
-                this.initing = false;
-              } else {
+    let secretKey = null;
+    const generateClientId = function() {
+      let result = 'omid-pwa-';
+      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const charactersLength = characters.length;
+      for (let i = 0; i < 16; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+    }
+    const connectMQTT = function() {
+      const config = this.decryptConfig(localStorage.getItem('config'), this.secretKey);
+      config.clientId = generateClientId();
+      // console.warn(config);
+      // console.warn(broker);
+      // console.log('▬ ▬ ▬', config);
+      const client = mqtt.connect(broker, config);
+      client.on("connect", (err) => {
+        if (!err) {
+          // subscribe();
+          // console.warn('MQTT CONNECTED to', broker);
+        } else {
+          // console.log('can not connect to mqtt', err);
+        }
+      });
+    }
+    const encryptConfig = function(plainText, secretKey) {
+      // console.log('ecn', plainText, secretKey);
+      var ciphertext = CryptoJS.AES.encrypt(plainText, secretKey);
+      return ciphertext.toString();
+    }
+    const decryptConfig = function(ciphertext, secretKey) {
+      // console.log('dec', ciphertext, secretKey);
+      var bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
+      var originalConfig = bytes.toString(CryptoJS.enc.Utf8);
+      return JSON.parse(originalConfig);
+    }
+    const connect = function() {
+      // console.warn('start connecting ...');
+      const config = this.decryptConfig(localStorage.getItem('config'), this.secretKey);
+      // console.warn(config);
+      // console.warn(this.broker);
+      const client = new Client({
+        brokerURL: this.broker,
+        connectHeaders: {
+          login: config.username,
+          passcode: config.password,
+        },
+        onConnect: () => {
+          client.subscribe(
+            config.username,
+            (message) => {
+              // console.log(`Received: ${message.body}`);
+              // Disconnect after receiving the message
+              this.emit('message', message.body);
+              client.deactivate();
+            });
+        },
+        onStompError: (frame) => {
+          // Will be called in case of error
+          // console.log('Broker reported error: ' + frame.headers['message']);
+          // console.warn('Additional details: ' + frame.body);
+          console.warn('connection error', frame.headers['message']);
+        },
+        onWebSocketClose: (event) => {
+          // Will be called when WebSocket closes
+          console.warn('connection closed: ', event);
+        },
+      });
+    
+      client.activate();
+    }
+    // public functions
+    this.publicFunction = function() {
+      privateFunction('from publicFunction');
+    }
+    this.init = function(firebaseConfig, vapidkey) {
+      return new Promise((resolve, reject) => {
+        if (!initing) {
+          initing = true;
+          const app = initializeApp(firebaseConfig);
+          Notification.requestPermission().then((permission) => {
+            if (permission === 'granted') {
+              const messaging = getMessaging();
+              getToken(messaging, { vapidKey: vapidkey }).then((currentToken) => {
+                if (currentToken) {
+                  fcm = currentToken;
+                  resolve(fcm);
+                  initing = false;
+                } else {
+                  reject('Can not init firebase.');
+                  initing = false;
+                }
+              }).catch((err) => {
+                // console.error('An error occurred while retrieving token. ', err);
                 reject('Can not init firebase.');
-                this.initing = false;
-              }
-            }).catch((err) => {
-              // console.error('An error occurred while retrieving token. ', err);
-              reject('Can not init firebase.');
-              this.initing = false;
-            });
-
-            // Handle incoming messages
-            onMessage(messaging, (payload) => {
-              // console.log('Message received. ', payload.notification);
-              if (checkConfig()) {
-                // connectMQTT(payload.notification);
-                connect();
-              }
-            });
-          } else {
-            // console.log('Unable to get permission to notify.');
-          }
-        });
-      }
-    });
-  }
-  setKey(key) {
-    this.secretKey = key;
-  }
-  getToken() {
-    return this.fcm;
-  }
-  // clientId generator
-  generateClientId() {
-    let result = 'omid-pwa-';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    for (let i = 0; i < 16; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-  }
-  encryptConfig(plainText, secretKey) {
-    var ciphertext = CryptoJS.AES.encrypt(plainText, secretKey);
-    return ciphertext.toString();
-  }
-  decryptConfig(ciphertext, secretKey) {
-    var bytes = CryptoJS.AES.decrypt(ciphertext, secretKey);
-    var originalConfig = bytes.toString(CryptoJS.enc.Utf8);
-    return JSON.parse(originalConfig);
-  }
-  setConfig(user, pass, broker) {
-    this.broker = broker;
-    const config = {
-      username: user,
-      password: pass,
-    };
-    // encrypt and store the config
-    const encConfig = this.encryptConfig(JSON.stringify(config), this.secretKey);
-    localStorage.setItem('config', encConfig);
-  }
-  // 2. check localStorage for config (omid notification data)
-  checkConfig() {
-    const mqttObject = localStorage.getItem('config');
-    let ready = false;
-    if (!mqttObject) {
-      ready = false;
-    } else {
-      this.config = this.decryptConfig(mqttObject, this.secretKey);
-      if (this.config.username && this.config.username !== '' && this.config.username !== null && this.config.username !== undefined &&
-        this.config.password && this.config.password !== '' && this.config.password !== null && this.config.password !== undefined &&
-        this.config.clientId && this.config.clientId !== '' && this.config.clientId !== null && this.config.clientId !== undefined) {
-        ready = true;
-      } else {
-        ready = false;
-      }
-    }
-    return ready;
-  }
-  // connect to server
-  connect() {
-    const config = this.decryptConfig(localStorage.getItem('config'), this.secretKey);
-    console.warn(config);
-    console.warn(this.broker);
-    const client = new Client({
-      brokerURL: this.broker,
-      connectHeaders: {
-        login: config.username,
-        passcode: config.password,
-      },
-      onConnect: () => {
-        client.subscribe(
-          config.username,
-          (message) => {
-            console.log(`Received: ${message.body}`);
-            // Disconnect after receiving the message
-            this.emit('message', message.body);
-            client.deactivate();
-          });
-      },
-      onStompError: (frame) => {
-        // Will be called in case of error
-        console.log('Broker reported error: ' + frame.headers['message']);
-        console.log('Additional details: ' + frame.body);
-      },
-      onWebSocketClose: (event) => {
-        // Will be called when WebSocket closes
-        console.log('WebSocket connection closed: ', event);
-      },
-    });
+                initing = false;
+              });
   
-    client.activate();
-  }
-  connectMQTT() {
-    const config = this.decryptConfig(localStorage.getItem('config'), this.secretKey);
-    config.clientId = this.generateClientId();
-    console.warn(config);
-    console.warn(this.broker);
-    console.log('▬ ▬ ▬', config);
-    this.client = mqtt.connect(this.broker, config);
-    this.client.on("connect", (err) => {
-      if (!err) {
-        // subscribe();
-        console.warn('MQTT CONNECTED to', this.broker);
+              // Handle incoming messages
+              onMessage(messaging, (payload) => {
+                // console.log('Message received. ', payload.notification);
+                if (this.checkConfig()) {
+                  // connectMQTT(payload.notification);
+                  connect();
+                }
+              });
+            } else {
+              // console.log('Unable to get permission to notify.');
+            }
+          });
+        }
+      });
+    }
+    this.checkConfig = function() { 
+      const configObject = localStorage.getItem('config');
+      // console.log('checkConfig', secretKey);
+      let ready = false;
+      if (!configObject) {
+        ready = false;
       } else {
-        console.log('can not connect to mqtt', err);
+        const c = decryptConfig(configObject, secretKey);
+        if (c.username && c.username !== '' && c.username !== null && c.username !== undefined &&
+          c.password && c.password !== '' && c.password !== null && c.password !== undefined) {
+          ready = true;
+        } else {
+          ready = false;
+        }
       }
-    });
-  }
-  // utils
-  getOs() {
-    const userAgent = window.navigator.userAgent;
-    const platform = window.navigator.platform;
-    const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-    const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-    const iosPlatforms = ['iPhone', 'iPad', 'iPod'];
-    let os = null;
-    if (macosPlatforms.indexOf(platform) !== -1) {
-      os = 'Mac OS' + '(' + platform + ')';
-    } else if (iosPlatforms.indexOf(platform) !== -1) {
-      os = 'iOS' + '(' + platform + ')';
-    } else if (windowsPlatforms.indexOf(platform) !== -1) {
-      os = 'Windows' + '(' + platform + ')';
-    } else if (/Android/.test(userAgent)) {
-      os = 'Android';
-    } else if (!os && /Linux/.test(platform)) {
-      os = 'Linux';
+      return ready;
     }
-    return os;
-  }
-  getBrowser() {
-    const [userAgent] = [navigator.userAgent];
-    let browserName = '';
-    if (/MSIE|Trident/.test(userAgent)) {
-      browserName = 'ie';
-    } else if (/Edge/.test(userAgent)) {
-      browserName = 'edge';
-    } else if (/CriOS/.test(userAgent)) {
-      browserName = 'chrome';
-    } else if (/Chrome/.test(userAgent)) {
-      browserName = 'chrome';
-    } else if (/Firefox/.test(userAgent)) {
-      browserName = 'firefox';
-    } else if (/Safari/.test(userAgent)) {
-      browserName = 'safari';
-    } else {
-      browserName = 'Unknown';
+    this.setConfig = function(user, pass, url) {
+      broker = url;
+      const config = {
+        username: user,
+        password: pass,
+      };
+      // encrypt and store the config
+      const encConfig = encryptConfig(JSON.stringify(config), secretKey);
+      localStorage.setItem('config', encConfig);
     }
-    return browserName;
+    this.getToken = function() {
+      return fcm;
+    }
+    this.setKey = function(key) {
+      secretKey = key;
+      // console.log(key, secretKey);
+    }
+    this.getOs = function() {
+      const userAgent = window.navigator.userAgent;
+      const platform = window.navigator.platform;
+      const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
+      const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
+      const iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+      let os = null;
+      if (macosPlatforms.indexOf(platform) !== -1) {
+        os = 'Mac OS' + ' (' + platform + ')';
+      } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = 'iOS' + ' (' + platform + ')';
+      } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = 'Windows' + ' (' + platform + ')';
+      } else if (/Android/.test(userAgent)) {
+        os = 'Android';
+      } else if (!os && /Linux/.test(platform)) {
+        os = 'Linux';
+      }
+      return os;
+    }
+    this.getBrowser = function() {
+      const [userAgent] = [navigator.userAgent];
+      let browserName = '';
+      if (/MSIE|Trident/.test(userAgent)) {
+        browserName = 'ie';
+      } else if (/Edge/.test(userAgent)) {
+        browserName = 'edge';
+      } else if (/CriOS/.test(userAgent)) {
+        browserName = 'chrome';
+      } else if (/Chrome/.test(userAgent)) {
+        browserName = 'chrome';
+      } else if (/Firefox/.test(userAgent)) {
+        browserName = 'firefox';
+      } else if (/Safari/.test(userAgent)) {
+        browserName = 'safari';
+      } else {
+        browserName = 'Unknown';
+      }
+      return browserName;
+    }
   }
 }
 module.exports = Arad;
